2cbfcaac5f8bd8e8c539d45f3849410b6b5c2ab7
happyfucheng <happyfucheng@126.com>
2015-06-08 16:18:56 +0800

    增加新的代码
    增加新的代码

---
+++ src/study/java/jdks/version1_5/VarArgs.java
@@ -0,0 +1,27 @@
+package study.java.jdks.version1_5;
+
+public class VarArgs {
+
+
+// 优先级高
+public static int add(int x,int y) {
+	return 0;
+}
+public static int add(int x,int...args) {
+	
+	int sum =x;
+	for(int arg:args) {
+		sum += arg;
+	}
+	return sum;
+}
+
+
+public static void main(String[] args) {
+	
+	System.out.println(add(2,6));
+	System.out.println(add(2));
+	System.out.println(add(2,3,5));
+	System.out.println(add(2,new int[]{3,5}));
+}
+}
